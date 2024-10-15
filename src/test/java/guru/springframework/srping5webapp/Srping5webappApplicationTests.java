c4f2bc187feba64e6171d5908397898908b855b4
ayselBatcoveanu <154974628+ayselBatcoveanu@users.noreply.github.com>
2024-10-15 13:49:33 +0300

    first commit

---
+++ src/test/java/guru/springframework/srping5webapp/Srping5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.srping5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srping5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
